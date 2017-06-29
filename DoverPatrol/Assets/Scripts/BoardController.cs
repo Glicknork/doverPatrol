23dd110626b0a2cc240c04e80c7146e9a19e8c71
Glicknork <glicknork@hotmail.com>
2017-06-29 20:32:25 +0100

    Initial commit

---
+++ DoverPatrol/Assets/Scripts/BoardController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardController : MonoBehaviour {
+
+    public Tiles[] tileArray;
+
+    [System.Serializable]
+    public class Tiles
+    {
+        public TileController[] tiles;
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
